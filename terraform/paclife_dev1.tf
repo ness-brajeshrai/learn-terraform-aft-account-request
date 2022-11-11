822fe5aff85a391303094a3ade1104bff4470b59
ness-brajeshrai <brajesh.rai@ness.com>
2022-11-12 02:13:05 +0530

    paclife dev account

---
+++ terraform/paclife_dev1.tf
@@ -0,0 +1,30 @@
+module "app_paclife_dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "AWS-MunichRe-packife-dev+1@ness.com"
+    AccountName               = "paclife_dev1"
+    ManagedOrganizationalUnit = "Apps"
+    SSOUserEmail              = "AWS-MunichRe-packife-dev+1@ness.com"
+    SSOUserFirstName          = "paclife_dev1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Project" = "MunicRe Apps"
+    "Owner"   = "Ness"
+    "Type"    = "Dev"
+    "OU"      = "PacLife"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Brajesh Rai"
+    change_reason       = "Provision a paclife dev account"
+  }
+
+  custom_fields = {
+    group = "paclife_dev1"
+  }
+
+  account_customizations_name = "paclife_dev1"
+}
